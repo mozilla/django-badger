b8957e4984b8cea78c7a3fe7db13b065f56c6ab4
Les Orchard <l.m.orchard@pobox.com>
2011-08-03 11:37:43 -0400

    Add admin for enhanced Badge model

--- badger_multiplayer/admin.py
+++ badger_multiplayer/admin.py
@@ -3,7 +3,23 @@ from django.contrib import admin
 from django import forms
 from django.db import models
 
+import badger.models
 from .models import (Badge, Nomination)
+
+
+class BadgerAdmin(admin.ModelAdmin):
+    list_display = ("title", )
+
+    filter_horizontal = ('prerequisites', )
+
+    formfield_overrides = {
+        models.ManyToManyField: {
+            "widget": forms.widgets.SelectMultiple(attrs={"size": 25})
+        }
+    }
+
+
+admin.site.register(Badge, BadgerAdmin)
 
 
 class NominationAdmin(admin.ModelAdmin):
