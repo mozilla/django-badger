56014bb6c565807af12aac85210489834bed8897
Les Orchard <l.m.orchard@pobox.com>
2011-09-27 01:50:31 -0400

    Tweaks to admin
    * Tweaks to fields in list display and editing forms
    * Showing Badge and Award images in list display

--- badger/admin.py
+++ badger/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 
 from django import forms
@@ -6,8 +7,26 @@ from django.db import models
 from .models import (Badge, Award, Progress)
 
 
+UPLOADS_URL = getattr(settings, 'BADGER_UPLOADS_URL',
+    '%suploads/' % getattr(settings, 'MEDIA_URL', '/media/'))
+
+
+def show_unicode(obj):
+    return unicode(obj)
+show_unicode.short_description = "Display"
+
+
+def show_image(obj):
+    img_url = "%s%s" % (UPLOADS_URL, obj.image)
+    return ('<a href="%s" target="_new"><img src="%s" width="64" height="64" /></a>' % 
+        (img_url, img_url))
+show_image.allow_tags = True
+show_image.short_description = "Image"
+
+
 class BadgerAdmin(admin.ModelAdmin):
-    list_display = ("title", )
+
+    list_display = ("title", "slug", "unique", "creator", show_image, "created", )
 
     filter_horizontal = ('prerequisites', )
 
@@ -18,18 +37,17 @@ class BadgerAdmin(admin.ModelAdmin):
     }
 
 
-admin.site.register(Badge, BadgerAdmin)
-
-
 class AwardAdmin(admin.ModelAdmin):
-    pass
 
+    list_display = (show_unicode, 'badge', 'user', 'creator', show_image, 'created', )
 
-admin.site.register(Award, AwardAdmin)
+    fields = ('badge', 'user', 'creator', )
 
 
 class ProgressAdmin(admin.ModelAdmin):
     pass
 
 
+admin.site.register(Badge, BadgerAdmin)
+admin.site.register(Award, AwardAdmin)
 admin.site.register(Progress, ProgressAdmin)
