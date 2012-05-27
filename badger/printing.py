0a5054017a8f360834a5062fd73ae0e32013521f
Les Orchard <me@lmorchard.com>
2012-05-26 23:36:13 -0400

    Fix #89: Get PDF printing working with Avery 22805 labels

--- badger/printing.py
+++ badger/printing.py
@@ -2,6 +2,7 @@
 """Quick and dirty render-to-PDF for badge award claim codes"""
 
 import logging
+import math
 import urllib
 import urllib2
 try:
@@ -9,6 +10,7 @@ try:
 except ImportError:
     from StringIO import StringIO
 
+from reportlab.pdfgen import canvas
 from reportlab.lib import pagesizes
 from reportlab.lib.units import inch
 from reportlab.platypus import (
@@ -20,6 +22,11 @@ from reportlab.rl_config import defaultPageSize
 from reportlab.lib.units import inch 
 from reportlab.lib.enums import TA_LEFT, TA_CENTER
 from reportlab.lib import colors
+from reportlab.lib import textsplit
+
+from reportlab.lib.utils import ImageReader
+
+from django.conf import settings
 
 from django.http import (HttpResponseRedirect, HttpResponse,
         HttpResponseForbidden, HttpResponseNotFound)
@@ -27,22 +34,183 @@ from django.http import (HttpResponseRedirect, HttpResponse,
 from django.utils.html import conditional_escape
 
 
-# Constants hard-coded to print onto Avery 5630 or Avery 5260 labels
-# TODO: Make formats / templates switchable
-top_margin = 0.5 * inch
-bottom_margin = 0.5 * inch
-left_margin = 0.1875 * inch
-right_margin = 0.1875 * inch
-width = 2.625 * inch
-height = 1.0 * inch
-vertical_spacing = 0 * inch
-horizontal_spacing = 0.125 * inch
-columns = 3
-rows = 10
-page_size = (8.5 * inch, 11.0 * inch)
-
-
 def render_claims_to_pdf(request, slug, claim_group, deferred_awards):
+    """Currently hard-coded to print to Avery 22805 labels"""
+
+    metrics = dict(
+        page_width=(8.5 * inch),
+        page_height=(11.0 * inch),
+
+        top_margin=(0.5 * inch),
+        left_margin=((25.0/32.0) * inch),
+        
+        horizontal_spacing=((5.0/16.0) * inch),
+        vertical_spacing=((13.0/64.0) * inch),
+
+        width=(1.5 * inch),
+        height=(1.5 * inch),
+    )
+
+    debug = (request.GET.get('debug', False) is not False)
+
+    pagesize = (metrics['page_width'], metrics['page_height'])
+    cols = int((metrics['page_width'] - metrics['left_margin']) / 
+               (metrics['width'] + metrics['horizontal_spacing']))
+    rows = int((metrics['page_height'] - metrics['top_margin']) /
+               (metrics['height'] + metrics['vertical_spacing']))
+    per_page = (cols * rows)
+    label_ct = len(deferred_awards)
+    page_ct = math.ceil(label_ct / per_page)
+
+    pages = [deferred_awards[x:x+(per_page)]
+             for x in range(0, label_ct, per_page)]
+
+    response = HttpResponse(content_type='application/pdf; charset=utf-8')
+    if not debug:
+        # If debugging, don't force download.
+        response['Content-Disposition'] = ('attachment; filename="%s-%s.pdf"' %
+                (slug.encode('utf-8', 'replace'), claim_group))
+
+    fout = StringIO()
+    c = canvas.Canvas(fout, pagesize=pagesize)
+
+    for page in pages:
+        c.translate(metrics['left_margin'],
+                    metrics['page_height']-metrics['top_margin'])
+
+        for row in range(0, rows, 1):
+            c.translate(0.0, 0 - metrics['height'])
+            c.saveState()
+
+            for col in range(0, cols, 1):
+
+                try:
+                    da = page.pop(0)
+                except IndexError:
+                    continue
+
+                c.saveState()
+                render_label(request, c, metrics, da)
+                c.restoreState()
+
+                dx = (metrics['width'] + metrics['horizontal_spacing'])
+                c.translate(dx, 0.0)
+
+            c.restoreState()
+            c.translate(0.0, 0 - metrics['vertical_spacing'])
+
+        c.showPage()
+
+    c.save()
+    response.write(fout.getvalue())
+    return response
+
+
+def render_label(request, c, metrics, da):
+    """Render a single label"""
+    badge = da.badge
+
+    badge_image_width = 1.1 * inch
+    badge_image_height = 1.1 * inch
+
+    qr_code_width = metrics['width'] - badge_image_width
+    qr_code_height = metrics['height'] - badge_image_height
+
+    fit_text(c, da.badge.title,
+             0.0, badge_image_height,
+             badge_image_width, qr_code_height)
+
+    c.saveState()
+    c.rotate(90)
+
+    code_height = qr_code_height * (0.45)
+    claim_height = qr_code_height - code_height
+
+    c.setFont("Courier", code_height)
+    c.drawCentredString(badge_image_width / 2.0,
+                        0 - metrics['width'] + claim_height,
+                        da.claim_code)
+    
+    text = """
+        <font name="Helvetica">Claim at</font> <font name="Courier">%s</font>
+    """ % (settings.SITE_TITLE)
+    fit_text(c, text,
+             0.0, 0-(1.5 * inch),
+             badge_image_width, claim_height)
+    
+    c.restoreState()
+
+    image_fin = badge.image.file
+    image_fin.open()
+    badge_img = ImageReader(StringIO(image_fin.read()))
+    
+    c.drawImage(badge_img, 
+                0.0 * inch, 0.0 * inch,
+                badge_image_width, badge_image_height)
+
+    try:
+        award_url = request.build_absolute_uri(da.get_claim_url())
+        qr_url = ("http://api.qrserver.com/v1/create-qr-code/?%s" %
+            urllib.urlencode({'size':'%sx%s' % (500, 500), 
+                              'data':award_url}))
+        #qr_url = ("http://chart.apis.google.com/chart?%s" %
+        #     urllib.urlencode({'chs':'%sx%s' % (250, 250), 
+        #                       'cht':'qr', 'chl':award_url, 'choe':'UTF-8'}))
+
+        qr_img = ImageReader(StringIO(urllib2.urlopen(qr_url).read()))
+
+        c.drawImage(qr_img, 
+                    badge_image_width, badge_image_height,
+                    qr_code_width, qr_code_height)
+
+    except Exception, e:
+        # Ignore issues in drawing the QR code - maybe show an error?
+        pass
+
+
+def fit_text(c, text, x, y, max_w, max_h, font_name='Helvetica', 
+             padding_w=4.5, padding_h=4.5, font_decrement=0.0625):
+    """Draw text, reducing font size until it fits with a given max width and
+    height."""
+
+    max_w -= (padding_w * 2.0)
+    max_h -= (padding_h * 2.0)
+
+    x += padding_w
+    y += padding_h
+
+    font_size = max_h
+
+    while font_size > 1.0:
+        ps = ParagraphStyle(name='text', alignment=TA_CENTER,
+                            fontName=font_name, fontSize=font_size,
+                            leading=font_size)
+        p = Paragraph(text, ps)
+        actual_w, actual_h = p.wrapOn(c, max_w, max_h)
+        if actual_h > max_h or actual_w > max_w:
+            font_size -= font_decrement
+        else:
+            y_pad = (max_h - actual_h) / 2
+            p.drawOn(c, x, y + y_pad)
+            return
+
+
+def _old_render_claims_to_pdf(request, slug, claim_group, deferred_awards):
+
+    # Constants hard-coded to print onto Avery 5630 or Avery 5260 labels
+    # TODO: Make formats / templates switchable
+    top_margin = 0.5 * inch
+    bottom_margin = 0.5 * inch
+    left_margin = 0.1875 * inch
+    right_margin = 0.1875 * inch
+    width = 2.625 * inch
+    height = 1.0 * inch
+    vertical_spacing = 0 * inch
+    horizontal_spacing = 0.125 * inch
+    columns = 3
+    rows = 10
+    page_size = (8.5 * inch, 11.0 * inch)
+
     debug = (request.GET.get('debug', False) is not False)
 
     response = HttpResponse(content_type='application/pdf; charset=utf-8')
@@ -120,8 +288,10 @@ def _real_render(request, stream, slug, claim_group, deferred_awards, debug,
         table_data = (
             (
                 (
-                    Image(badge_img, 0.75 * inch * scale_factor, 0.75 * inch * scale_factor),
-                    resize_para(badge.title, max_width=1.75 * inch * scale_factor),
+                    Image(badge_img, 0.75 * inch * scale_factor,
+                          0.75 * inch * scale_factor),
+                    resize_para(badge.title, 
+                          max_width=1.75 * inch * scale_factor),
                 ),
                 (
                     Image(qr_img, 0.6 * inch, 0.6 * inch),
