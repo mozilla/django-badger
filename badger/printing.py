72f363cdc050e8326731ad0de049ec0cd10aa2c3
Les Orchard <me@lmorchard.com>
2012-05-31 21:35:23 -0400

    Fix #75: Use PyQRNative to generate QR codes, when available

--- badger/printing.py
+++ badger/printing.py
@@ -47,8 +47,6 @@ def render_claims_to_pdf(request, slug, claim_group, deferred_awards):
         horizontal_spacing=((5.0/16.0) * inch),
         vertical_spacing=((13.0/64.0) * inch),
 
-        qr_edge_padding=2,
-
         width=(1.5 * inch),
         height=(1.5 * inch),
     )
@@ -73,6 +71,8 @@ def render_claims_to_pdf(request, slug, claim_group, deferred_awards):
         response['Content-Disposition'] = ('attachment; filename="%s-%s.pdf"' %
                 (slug.encode('utf-8', 'replace'), claim_group))
 
+    badge_img = None
+
     fout = StringIO()
     c = canvas.Canvas(fout, pagesize=pagesize)
 
@@ -91,8 +91,13 @@ def render_claims_to_pdf(request, slug, claim_group, deferred_awards):
                 except IndexError:
                     continue
 
+                if not badge_img:
+                    image_fin = da.badge.image.file
+                    image_fin.open()
+                    badge_img = ImageReader(StringIO(image_fin.read()))
+
                 c.saveState()
-                render_label(request, c, metrics, da, debug)
+                render_label(request, c, metrics, da, badge_img, debug)
                 c.restoreState()
 
                 dx = (metrics['width'] + metrics['horizontal_spacing'])
@@ -108,7 +113,7 @@ def render_claims_to_pdf(request, slug, claim_group, deferred_awards):
     return response
 
 
-def render_label(request, c, metrics, da, debug):
+def render_label(request, c, metrics, da, badge_img, debug):
     """Render a single label"""
     badge = da.badge
 
@@ -150,31 +155,41 @@ def render_label(request, c, metrics, da, debug):
              badge_image_width, claim_height)
     
     c.restoreState()
-
-    image_fin = badge.image.file
-    image_fin.open()
-    badge_img = ImageReader(StringIO(image_fin.read()))
     
     c.drawImage(badge_img, 
                 0.0 * inch, 0.0 * inch,
                 badge_image_width, badge_image_height)
 
+    # Attempt to build a QR code image for the claim URL
+    claim_url = request.build_absolute_uri(da.get_claim_url())
+    qr_img = None
     try:
-        award_url = request.build_absolute_uri(da.get_claim_url())
-        qr_url = ("http://api.qrserver.com/v1/create-qr-code/?%s" %
-            urllib.urlencode({'size':'%sx%s' % (500, 500), 
-                              'data':award_url}))
+        # Try using PyQRNative: http://code.google.com/p/pyqrnative/
+        # badg.us should have this in vendor-local
+        from PyQRNative import QRCode, QRErrorCorrectLevel
+        qr = QRCode(3, QRErrorCorrectLevel.L) # TODO: Good-enough settings?
+        qr.addData(claim_url)
+        qr.make()
+        qr_img = ImageReader(qr.makeImage())
 
-        qr_img = ImageReader(StringIO(urllib2.urlopen(qr_url).read()))
+    except ImportError:
+        try:
+            # Hmm, if we don't have PyQRNative, then try abusing this web
+            # service. Should be fine for low volumes.
+            qr_url = ("http://api.qrserver.com/v1/create-qr-code/?%s" %
+                urllib.urlencode({'size':'%sx%s' % (500, 500), 
+                                  'data':claim_url}))
 
+            qr_img = ImageReader(StringIO(urllib2.urlopen(qr_url).read()))
+
+        except Exception, e:
+            # Ignore issues in drawing the QR code - maybe show an error?
+            pass
+
+    if qr_img:
         c.drawImage(qr_img, 
                     badge_image_width, badge_image_height,
-                    qr_code_width - metrics['qr_edge_padding'],
-                    qr_code_height - metrics['qr_edge_padding'])
-
-    except Exception, e:
-        # Ignore issues in drawing the QR code - maybe show an error?
-        pass
+                    qr_code_width, qr_code_height)
 
 
 def fit_text(c, text, x, y, max_w, max_h, font_name='Helvetica', 
@@ -202,146 +217,3 @@ def fit_text(c, text, x, y, max_w, max_h, font_name='Helvetica',
             y_pad = (max_h - actual_h) / 2
             p.drawOn(c, x, y + y_pad)
             return
-
-
-def _old_render_claims_to_pdf(request, slug, claim_group, deferred_awards):
-
-    # Constants hard-coded to print onto Avery 5630 or Avery 5260 labels
-    # TODO: Make formats / templates switchable
-    top_margin = 0.5 * inch
-    bottom_margin = 0.5 * inch
-    left_margin = 0.1875 * inch
-    right_margin = 0.1875 * inch
-    width = 2.625 * inch
-    height = 1.0 * inch
-    vertical_spacing = 0 * inch
-    horizontal_spacing = 0.125 * inch
-    columns = 3
-    rows = 10
-    page_size = (8.5 * inch, 11.0 * inch)
-
-    debug = (request.GET.get('debug', False) is not False)
-
-    response = HttpResponse(content_type='application/pdf; charset=utf-8')
-    if not debug:
-        # If debugging, don't force download.
-        response['Content-Disposition'] = ('attachment; filename="%s-%s.pdf"' %
-                (slug.encode('utf-8', 'replace'), claim_group))
-
-    # HACK: If layout fails, it's most likely because of the title.  I am so
-    # ashamed of this, but this reduces the size of badge image and title by 5%
-    # for each error, until the error goes away. For the love of Bob, fix this
-    # good & proper.
-    scale_factor = 1.0
-    while scale_factor > 0.1:
-        try:
-            fout = StringIO()
-            _real_render(request, fout, slug, claim_group, deferred_awards,
-                         debug, scale_factor)
-            break
-        except LayoutError, e:
-            scale_factor -= 0.05
-
-    response.write(fout.getvalue())
-    return response
-
-
-def _real_render(request, stream, slug, claim_group, deferred_awards, debug,
-                 scale_factor=1.0):
-    """Render the pages of badge codes."""
-    doc = BaseDocTemplate(stream, topMargin=0, bottomMargin=0,
-                          leftMargin=0, rightMargin=0, allowSplitting=0)
-    
-    if debug: show_boundary = 1
-    else: show_boundary = 0
-
-    # Build frames for labels in the template
-    frames = []
-    for r_idx in range(0, rows):
-        for c_idx in range(0, columns):
-            left_pos = left_margin + (c_idx * (width + horizontal_spacing))
-            top_pos = (top_margin + (r_idx * (height + vertical_spacing)))
-            frames.append(Frame(
-                left_pos, top_pos, width, height,
-                leftPadding=0, rightPadding=0,
-                bottomPadding=0, topPadding=0,
-                showBoundary=show_boundary
-            ))
-
-    # Add the template to the page.
-    template = PageTemplate(pagesize=page_size, frames=frames)
-    doc.addPageTemplates(template)
-
-    # Fill out the template with claim codes.
-    items = []
-    for da in deferred_awards:
-        badge = da.badge
-        award_url = request.build_absolute_uri(da.get_claim_url())
-
-        image_fin = badge.image.file
-        image_fin.open()
-        badge_img = StringIO(image_fin.read())
-
-        # TODO: Stop abusing the Google Charts API and get our own QR code
-        # baking on premises.
-        try:
-            qr_url = ("http://api.qrserver.com/v1/create-qr-code/?%s" %
-                urllib.urlencode({'size':'%sx%s' % (250, 250), 
-                                  'data':award_url}))
-            qr_img = StringIO(urllib2.urlopen(qr_url).read())
-        except Exception, e:
-            return HttpResponse('QR code generation failed: %s' % e,
-                                status=500)
-
-        # Build the badge label out as a table...
-        table_data = (
-            (
-                (
-                    Image(badge_img, 0.75 * inch * scale_factor,
-                          0.75 * inch * scale_factor),
-                    resize_para(badge.title, 
-                          max_width=1.75 * inch * scale_factor),
-                ),
-                (
-                    Image(qr_img, 0.6 * inch, 0.6 * inch),
-                    Paragraph(request.build_absolute_uri('/'), ParagraphStyle(
-                        name='normal', alignment=TA_CENTER,
-                        fontName='Helvetica', fontSize=8, leading=8)),
-                    Paragraph(da.claim_code.upper(), ParagraphStyle(
-                        name='code', alignment=TA_CENTER,
-                        fontName='Courier', fontSize=11, leading=11)),
-                ),
-            ),
-        )
-
-        table_style = (
-            ('ALIGN', (0,0), (-1,-1), 'CENTER'),  
-        )
-
-        if debug:
-            table_style = table_style + (
-                ('GRID', (0,0), (-1,-1), 1, colors.black),  
-            )
-
-        items.append(Table(table_data, style=table_style))
-        items.append(FrameBreak())
-
-    doc.build(items)
-
-
-def resize_para(str, max_size=10.0, min_size=2.0, max_width=(1.25*inch),
-                font_name='Helvetica', alignment=TA_CENTER):
-    """Produce a paragraph, reducing font size until it fits in the max width"""
-    size = max_size
-    while size > min_size:
-        # HACK: Use a preformatted object so that minWidth() fomes up with
-        # non-wrapped width. This just feels so dirty, dirty, but it works
-        style = ParagraphStyle(name='Size %s' % size,
-                               alignment=alignment, fontName=font_name,
-                               fontSize=size, leading=size+0.25)
-        para = Preformatted(str, style)
-        if para.minWidth() <= max_width:
-            para = Paragraph(str, style)
-            break
-        size -= 0.125
-    return para
