b5e3dd95dc5f9ceafded3499aa3715196abb263d
Will Kahn-Greene <willkg@mozilla.com>
2013-08-23 23:38:04 -0400

    Delete commented out urlparams code
    @lmorchard said it was ok to nuke this from orbit. Done.

--- badger/helpers.py
+++ badger/helpers.py
@@ -88,37 +88,3 @@ def nominations_pending_acceptance(user):
     return Nomination.objects.filter(nominee=user,
                                      approver__isnull=False,
                                      accepted=False)
-
-
-# FIXME - This code is broken because smart_str doesn't exist in the namespace
-# Since it's not used anywhere in django-badger and I'm not sure whether
-# deleting it is ok or not, I'm commenting it out.
-#
-# @register.filter
-# def urlparams(url_, hash=None, **query):
-#     """Add a fragment and/or query paramaters to a URL.
-# 
-#     New query params will be appended to exising parameters, except duplicate
-#     names, which will be replaced.
-#     """
-#     url = urlparse.urlparse(url_)
-#     fragment = hash if hash is not None else url.fragment
-# 
-#     # Use dict(parse_qsl) so we don't get lists of values.
-#     q = url.query
-#     query_dict = dict(urlparse.parse_qsl(smart_str(q))) if q else {}
-#     query_dict.update((k, v) for k, v in query.items())
-# 
-#     query_string = _urlencode([(k, v) for k, v in query_dict.items()
-#                                if v is not None])
-#     new = urlparse.ParseResult(url.scheme, url.netloc, url.path, url.params,
-#                                query_string, fragment)
-#     return new.geturl()
-# 
-# 
-# def _urlencode(items):
-#     """A Unicode-safe URLencoder."""
-#     try:
-#         return urllib.urlencode(items)
-#     except UnicodeEncodeError:
-#         return urllib.urlencode([(k, smart_str(v)) for k, v in items])
